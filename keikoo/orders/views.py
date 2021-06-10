da022b809bdc99265a8e944d866b5a229367bf10
bulalu <elishabulalu031@gmail.com>
2021-06-10 11:53:00 +0300

    orders into CSV
    tried to added an invoice feature but got OS errors, I'll next time

--- keikoo/orders/views.py
+++ keikoo/orders/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
-from .models import OrderItem
+from .models import OrderItem, Order
 from .forms import OrderCreateForm
 from cart.cart import Cart
 from .tasks import order_created
+from django.contrib.admin.views.decorators import staff_member_required
+from django.shortcuts import get_object_or_404
+from django.conf import settings
+from django.http import HttpResponse
+from django.template.loader import render_to_string
 
-# Create your views here.
+
+
+
+@staff_member_required
+def admin_order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    return render(request,'admin/orders/order/detail.html', {'order':order})
+
 
 def order_create(request):
     cart = Cart(request)
